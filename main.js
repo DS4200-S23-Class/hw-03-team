865d05b2d029d6df060ae631a1a552a5f87c85d9
liangv21 <liang.va@northeastern.edu>
2023-02-07 16:36:39 -0500

    created font changing button

---
+++ main.js
@@ -0,0 +1,32 @@
+// Initialize array with different font families
+const fontsList = ["Times New Roman", "Comic Sans MS", "Chalkduster",
+    "Brush Script MT", "Bradley Hand", "Apple Chancery",
+    "Copperplate", "Courier New", "Didot", "Geneva", "Gill Sans",
+    "Luminari", "Marker Felt", "Arial"];
+
+// Initialize counter to iterate through array
+let counter = 0;
+
+// Create text that will remain the same throughout function
+const fontText = " is a nice font, but...";
+
+
+// Function used to change the font on click
+function changeFont() {
+
+    if (counter >= fontsList.length) {
+        counter = 1
+    };
+    
+    // Add to counter
+    counter++;
+
+    // Change the font of the website in order of the list
+    document.body.style.fontFamily = fontsList[counter];
+
+    // Create new statement for ahove button
+    let finalStatement = fontsList[counter].concat(fontText)
+
+    // Return the final statement and updates html
+    document.getElementById("button-text").innerHTML = finalStatement;
+};
